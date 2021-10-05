ffacdd1cac45197704c6077395d849d585d76b7f
smonajemi <sina.monajemi@me.com>
2021-10-05 00:59:26 -0400

    commit

--- source/repositories/user.repositories.ts
+++ source/repositories/user.repositories.ts
@@ -14,22 +14,28 @@ const columns = [
     'updated_at',
     'deleted_at'
 ]
+
 export const fetchUsers = async (): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
     .whereNull('deleted_at')
     .select(columns)
+
 export const fetchUserById = async (userId: string): Promise<UserEntity> => await db<UserEntity>(TABLE_NAME)
     .whereNull('deleted_at')
     .where('id', userId).first(columns)
+
 export const fetchUserByEmail = async (email: string): Promise<UserEntity> => await db<UserEntity>(TABLE_NAME)
     .whereNull('deleted_at')
     .where('username', email)
     .first(columns)
 
-export const createUser = async (user: UserEntity): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME).insert({...user, created_at: db.raw('now()'), updated_at: db.raw('now()')}, columns)
+export const createUser = async (user: UserEntity): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
+    .insert({...user, created_at: db.raw('now()'), updated_at: db.raw('now()')}, columns)
+
 export const updateUser = async (userId: string ,user: UserEntity): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
     .where('id', userId)
     .whereNull('deleted_at')
     .update({...user, updated_at: db.raw('now()')}, columns)
+
 export const deleteUser = async (userId: string, user: UserEntity): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
     .where('id', userId)
     .update({updated_at: db.raw('now()'), deleted_at: db.raw('now()')}, columns)
