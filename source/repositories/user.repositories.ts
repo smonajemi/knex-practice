076994ab2eb580fad8c4f11d279219f44ec67492
smonajemi <sina.monajemi@me.com>
2021-10-05 01:43:19 -0400

    refactored

--- source/repositories/user.repositories.ts
+++ source/repositories/user.repositories.ts
@@ -15,27 +15,20 @@ const columns = [
 ]
 
 export const fetchUsers = async (): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
-    .whereNull('deleted_at')
-    .select(columns)
+    .whereNull('deleted_at').select(columns)
 
 export const fetchUserById = async (userId: string): Promise<UserEntity> => await db<UserEntity>(TABLE_NAME)
-    .whereNull('deleted_at')
-    .where('id', userId).first(columns)
+    .whereNull('deleted_at').where('id', userId).first(columns)
 
 export const fetchUserByEmail = async (email: string): Promise<UserEntity> => await db<UserEntity>(TABLE_NAME)
-    .whereNull('deleted_at')
-    .where('username', email)
-    .first(columns)
+    .whereNull('deleted_at').where('username', email).first(columns)
 
 export const createUser = async (user: UserEntity): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
     .insert({...user, created_at: db.raw('now()'), updated_at: db.raw('now()')}, columns)
 
 export const updateUser = async (userId: string ,user: UserEntity): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
-    .where('id', userId)
-    .whereNull('deleted_at')
-    .update({...user, updated_at: db.raw('now()')}, columns)
+    .where('id', userId).whereNull('deleted_at').update({...user, updated_at: db.raw('now()')}, columns)
 
 export const deleteUser = async (userId: string): Promise<UserEntity[]> => await db<UserEntity>(TABLE_NAME)
-    .where('id', userId)
-    .update({updated_at: db.raw('now()'), deleted_at: db.raw('now()'), is_deleted: true}, columns)
+    .where('id', userId).update({updated_at: db.raw('now()'), deleted_at: db.raw('now()'), is_deleted: true}, columns)
     
